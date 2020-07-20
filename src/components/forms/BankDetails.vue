19e09db4312df0be55ebeec5bdce1e9882ff9e95
peter chege <peterchege442@gmail.com>
2020-07-20 16:55:06 +0300

    bank forms

--- src/components/forms/BankDetails.vue
+++ src/components/forms/BankDetails.vue
@@ -3,7 +3,7 @@
        
           <v-row class="mx-2 py-7 px-10" >
   
-            <v-col cols="12" md="4" sm="12">
+            <v-col cols="12" md="6" sm="12">
               <v-text-field
                 prepend-icon="mdi-account"
                 label="Account Holder"
@@ -20,42 +20,6 @@
                 v-model="department"
                 :rules="InputRulesDept">
                 </v-select>
-            </v-col>
-
-
-            <v-col cols="12" md="4" sm="12">
-              <v-text-field
-                prepend-icon="mdi-phone"
-                label="Mobile Number"
-                v-model="phoneNumber"
-                :rules="InputRulesphoneNumber">
-               </v-text-field>
-            </v-col>
-            <v-col cols="12" md="4" sm="12">
-              <v-text-field
-                prepend-icon="mdi-phone"
-                label="Telephone Number"
-                v-model="phoneNumber"
-                :rules="InputRulesphoneNumber">
-               </v-text-field>
-            </v-col>
-            <v-col cols="12" md="4" sm="12" >
-              <v-text-field
-                prepend-icon="mdi-folder"
-                label="KRA PIN"
-                v-model="kra"
-                :rules="InputRuleskra">
-               </v-text-field>
-            </v-col>
-
-            <v-col cols="12" md="4" sm="12">
-              <v-text-field
-                type="tel"
-                prepend-icon="mdi-folder-account"
-                label="ID / Passport Number"
-                v-model="vat"
-                :rules="InputRulesbankVAT"
-              ></v-text-field>
             </v-col>
 
             <v-col cols="12" md="4" sm="12">
@@ -85,15 +49,6 @@
               ></v-text-field>
             </v-col>
 
-            <v-col cols="12" md="6" sm="12">
-              <v-text-field
-                type="tel"
-                prepend-icon="mdi-badge-account-horizontal"
-                label="Physical Address"
-                v-model="address"
-                :rules="InputRulesAddress"
-              ></v-text-field>
-            </v-col>
 
           </v-row>
        
