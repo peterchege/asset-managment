a2b705d2d72e50463e452d73fba256eb46b2847a
peter chege <peterchege442@gmail.com>
2020-07-20 14:59:50 +0300

    application form

--- src/App.vue
+++ src/App.vue
@@ -7,54 +7,37 @@
     >
       <div class="d-flex align-center">
         <v-img
-          alt="Vuetify Logo"
+          alt="APA Insurance"
           class="shrink mr-2"
           contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
+          src="https://apainsurance.org/img/logon.png"
           transition="scale-transition"
-          width="40"
+          width="80"
         />
 
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
       </div>
 
-      <v-spacer></v-spacer>
-
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
     </v-app-bar>
 
     <v-content>
-      <HelloWorld/>
+      <v-container align="center" justify="center" class="mt-10">
+          <app-form/>
+      </v-container>
     </v-content>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld';
 
 export default {
   name: 'App',
 
   components: {
-    HelloWorld,
+    appForm:() => import ('@/views/ApplicationForm'),
   },
 
   data: () => ({
-    //
+    
   }),
 };
 </script>
--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -4,4 +4,17 @@ import Vuetify from 'vuetify/lib';
 Vue.use(Vuetify);
 
 export default new Vuetify({
+    theme: {
+        themes: {
+            light: {
+                primary: '#034EA2',
+                secondary: '#F8AB10',
+                accent: '#EFEFEF',
+                error: '#FF5252',
+                info: '#F8F9FB',
+                success: '#4CAF50',
+                warning: '#FFC107'
+            },
+        },
+    },
 });
