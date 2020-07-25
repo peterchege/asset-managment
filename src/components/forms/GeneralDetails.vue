f4f9d80808bffa7cf751b2f739d4acfd314cfe8b
peter chege <peterchege442@gmail.com>
2020-07-25 18:25:09 +0300

    account number

--- src/components/forms/GeneralDetails.vue
+++ src/components/forms/GeneralDetails.vue
@@ -3,33 +3,61 @@
     <div>
 
     <v-card outlined  class="my-7 pa-5"  >
-        <v-card-title>General Details <span class="red--text"> ( Please tick where Applicable ) </span> </v-card-title>
         <v-row class="ma-auto">
-            <v-col cols="12" md="4">
-                <v-checkbox v-model="selected" label="Sale of an Investment portfolio" value="investment portfolio"></v-checkbox>
+            <v-col cols="12" md="6">
+                <v-card-title>Preffered mode of recieving monthly correspondence :</v-card-title>
             </v-col>
-            <v-col cols="12" md="4">
-                <v-checkbox v-model="selected" label="Gift or inheritance" value="Gift or inheritance"></v-checkbox>
+            <v-col cols="12" md="3">
+                <v-checkbox v-model="selected" label="E-mail" value="email"></v-checkbox>
             </v-col>
-            <v-col cols="12" md="4">
-                <v-checkbox v-model="selected" label="Disposal of propert" value="Disposal of propert"></v-checkbox>
+            <v-col cols="12" md="3">
+                <v-checkbox v-model="selected" label="Post" value="post"></v-checkbox>
             </v-col>
         </v-row> 
 
         <v-row class="ma-auto">
-            <v-col cols="12" md="4">
-                <v-checkbox v-model="selected" label="Savings" value="Savings"></v-checkbox>
+            <v-col cols="12" md="6">
+                <v-card-title>Have you previously invested in the Apollo Unit Trust Fund :</v-card-title>
+                <v-card-subtitle class="pl-5">(if yes quote the account Number)</v-card-subtitle>
             </v-col>
-            <v-col cols="12" md="4">
-                <v-checkbox v-model="show" label="others (please state)" value="true"></v-checkbox>
+            <v-col cols="12" md="3">
+                <v-checkbox v-model="selected" label="Yes" value="yes"></v-checkbox>
+            </v-col>
+            <v-col cols="12" md="3">
+                <v-checkbox v-model="show" label="No" value="no"></v-checkbox>
                 <v-text-field
                     v-if="show"
-                    v-model="othersDetails"
+                    label="Account Number"
+                    v-model="accountNumber"
                     outlined
-                    :disabled="disable"
                 ></v-text-field>
             </v-col>
         </v-row> 
+
+        <v-row class="ma-auto">
+            <v-col cols="12" md="6">
+                <v-card-title>Preffered mode of recieving monthly correspondence :</v-card-title>
+            </v-col>
+            <v-col cols="12" md="3">
+                <v-checkbox v-model="selected" label="E-mail" value="email"></v-checkbox>
+            </v-col>
+            <v-col cols="12" md="3">
+                <v-checkbox v-model="selected" label="Post" value="post"></v-checkbox>
+            </v-col>
+        </v-row> 
+
+        <v-row class="ma-auto">
+            <v-col cols="12" md="6">
+                <v-card-title>Preffered mode of recieving monthly correspondence :</v-card-title>
+            </v-col>
+            <v-col cols="12" md="3">
+                <v-checkbox v-model="selected" label="E-mail" value="email"></v-checkbox>
+            </v-col>
+            <v-col cols="12" md="3">
+                <v-checkbox v-model="selected" label="Post" value="post"></v-checkbox>
+            </v-col>
+        </v-row> 
+        
     </v-card>            
     </div>
 </template>
@@ -40,7 +68,8 @@
       return {
         selected: [],
         show: false,
-        othersDetails:''
+        othersDetails:'',
+        accountNumber:'',
       }
     },
   }
